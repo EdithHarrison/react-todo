1ff298a6fbb0c62cb81b743e2db040ad4cb69167
Edith Harrison <edithharrison@ediths-laptop.lan>
2024-06-30 23:47:05 -0400

    Props State And Handlers

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
+import TodoListItem from './TodoListItem';
 
-const todoList = [
-  { id: 1, title: 'The Little Mermaid' },
-  { id: 2, title: 'Toy Story' },
-  { id: 3, title: 'Moana' }
-];
-
-const TodoList = () => {
-    return (
-        <ul>
-            {todoList.map(item => (
-                <li key={item.id}>{item.title}</li>
-            ))}
-        </ul>
-    );
+const TodoList = (props) => {
+  return (
+    <ul>
+      {props.todos.map(item => (
+        <TodoListItem key={item.id} todo={item} />
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
